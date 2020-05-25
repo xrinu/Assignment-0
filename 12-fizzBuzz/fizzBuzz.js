6287d61612bd35fc8f8ea50fb8d8d0bdd41ad9d8
xrinu <rinuxavier96@gmail.com>
2020-05-25 18:23:43 -0400

    completed tst-12

--- 12-fizzBuzz/fizzBuzz.js
+++ 12-fizzBuzz/fizzBuzz.js
@@ -1,5 +1,31 @@
 function fizzBuzz(start, end) {
-  // Insert code here;
+	var displayArr = new Array();
+
+	for(var i=start; i<= end; i++)
+	{
+		
+		 if(i%3==0 && i%5==0)
+		{
+			displayArr.push("FizzBuzz");
+		}
+		else if(i%3==0)
+		{
+			displayArr.push("Fizz");
+		}
+		 else if(i%5==0)
+		{
+			displayArr.push("Buzz");
+		}
+		else
+		{
+			displayArr.push(i);
+
+		}
+		
+	}
+
+	return displayArr;   
+  
 }
 
 // Do not edit this line;
