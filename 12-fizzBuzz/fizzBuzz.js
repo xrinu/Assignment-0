613c591f3dab5e5bc4d1c2a02f6c212380ccec70
xrinu <rinuxavier96@gmail.com>
2020-05-25 18:27:01 -0400

    modified test-12: added comments

--- 12-fizzBuzz/fizzBuzz.js
+++ 12-fizzBuzz/fizzBuzz.js
@@ -3,19 +3,22 @@ function fizzBuzz(start, end) {
 
 	for(var i=start; i<= end; i++)
 	{
-		
+		//multiples of 3 and 5
 		 if(i%3==0 && i%5==0)
 		{
 			displayArr.push("FizzBuzz");
 		}
+		//multipples of 3
 		else if(i%3==0)
 		{
 			displayArr.push("Fizz");
 		}
+		//multiples of 5
 		 else if(i%5==0)
 		{
 			displayArr.push("Buzz");
 		}
+		//to insert the number:
 		else
 		{
 			displayArr.push(i);
