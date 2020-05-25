e3dfea809259c4f0f363e8711f8ca1c4993d3cc0
xrinu <rinuxavier96@gmail.com>
2020-05-25 14:07:21 -0400

    completed test-11

--- 11-sumOfMinimumAndMaximum/sumOfMinimumAndMaximum.js
+++ 11-sumOfMinimumAndMaximum/sumOfMinimumAndMaximum.js
@@ -1,6 +1,29 @@
 function sumOfMinimumAndMaximum(nums) {
-  // Insert code here;
-}
+  var sum=0;
+  var min = nums[0];
+  var max = nums[0];
+  
+  
+  for(var i =0; i <nums.length; i++)
+  {  
+  	//to find lowest number
+  	if(min >= nums[i+1])
+  	{
+  		min = nums[i+1];  
+  	}
+
+  	//to find highest number
+  	if(max <=  nums[i+1])
+  	{
+  		max = nums[i+1];
+  	}
+
+  }
+
+sum = min + max;
+return sum;
+
+}//end of function
 
 // Do not edit this line;
 module.exports = sumOfMinimumAndMaximum;
