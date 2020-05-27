f2b844a002d8845133cb1dec0573a67ef775d70b
xrinu <rinuxavier96@gmail.com>
2020-05-26 23:17:53 -0400

    completed test-19

--- 18-pairSum/pairSum.js
+++ 18-pairSum/pairSum.js
@@ -1,5 +1,19 @@
 function pairSum(nums, target) {
-  // Insert code here;
+	if(nums.length <2)    //if less than two numbers
+	{
+		return error;
+	}
+	else{
+  for(var i=0; i<nums.length; i++)
+  {
+  	for(var j= i+1; j<nums.length; j++)
+  	{
+  		if(nums[i]+nums[j]==target)    //if equal to target
+  			return true;
+  	}
+  }
+}
+return false;      //if no pair sum is equal to target
 }
 
 // Do not edit this line;
--- 19-binarySearch/binarySearch.js
+++ 19-binarySearch/binarySearch.js
@@ -4,10 +4,28 @@ class MySolution {
   }
 
   binarySearch(nums, target) {
-    // Insert code here;
-  }
+var start = 0;    //set to first index
+var end = nums.length-1;   //set to last index
+
+while(start <= end)
+{
+	//to take the middle index of the array
+	var mid = Math.floor((start+end)/2);
+
+	if(nums[mid]==target)    //check if target is equal to mid index value
+		return true;
+
+	else if(nums[mid] < target)
+		start = mid + 1;   //move right by one index
+
+	else  if(nums[mid] > target)
+		end = mid-1;       //move left by one index
 }
 
+return false;
+}
+
+ }
 // Do not edit this line;
 let StudentSolution = MySolution;
 module.exports = StudentSolution;
